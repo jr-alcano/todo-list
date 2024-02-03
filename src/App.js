d6a587d35a6f975ef5543cf04fcbf29a436865d6
jalcan9 <jalcan9@uic.edu>
2024-02-02 21:12:57 -0600

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react' //import react and useState, useState lets variables maintain state during re-renders
+import axios from 'axios'
 
 function App() {
+  
+  const [list, setList] = useState ([]); //list variable, useState returns the state of 'list' and the function that updates it, 'setList'
+  const [input, setInput] = useState(""); //input variable, useState returns 'input, and function 'setInput'
+
+  const fetchImage = async (query) => {
+    const apiKey = process.env.REACT_APP_GOOGLE_API_KEY;
+    const engineId = process.env.REACT_APP_GOOGLE_ENGINE_ID;
+    const url = `https://www.googleapis.com/customsearch/v1?key=${apiKey}&cx=${engineId}&q=${encodeURIComponent(query)}&searchType=image`;
+  
+    try {
+      const response = await axios.get(url);
+      const imageUrl = response.data.items[0].link; // Get the first image's link
+      return imageUrl;
+    } catch (error) {
+      console.error("Failed to fetch image:", error);
+      return null; // or a default image URL
+    }
+  };
+
+  const addTodo = async (todo) => { 
+    const imageUrl = await fetchImage(todo)
+
+    const newTodo = {
+      id: Math.random(),
+      todo: todo, // first todo is the property of the javascript object, second is the value assigned to that property
+      image: imageUrl
+    }
+
+    setList([...list, newTodo])
+
+    setInput("")
+  }
+
+  const deleteTodo = (id) => {
+    const newList = list.filter((todo) => todo.id !== id)
+    setList(newList)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Todo List</h1>
+      <input
+        type = "text"
+        value = {input}
+        onChange = {(e) => setInput(e.target.value)}
+        />
+      <button onClick = {() => addTodo(input)} >Add</button>
+      <ul>
+       {list.map ((todo) => (
+        <li key = {todo.id}>
+          {todo.todo}
+          {todo.image && <img src={todo.image} alt={todo.todo} />}
+          <button onClick = {() => deleteTodo(todo.id)}>&times;</button>
+        </li>
+       ))}
+      </ul>
     </div>
   );
 }
 
-export default App;
+export default App
